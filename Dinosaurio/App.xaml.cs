82497a4acf43fc03c33f2454cff1b25b14a10d34
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-07-03 21:32:12 -0600

    Agregar archivos de proyecto.

---
+++ Dinosaurio/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Dinosaurio;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Dinosaurio/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Dinosaurio;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void BtnCalcular_Clicked(object sender, EventArgs e)
+	{
+			if (!string.IsNullOrEmpty(peso.Text) && !string.IsNullOrEmpty(altura.Text))
+			{
+				var dato1 = Convert.ToDouble(peso.Text);
+				var dato2 = Convert.ToDouble(altura.Text);
+
+				var libr = dato1 * 2.2;
+				libras.Text = Convert.ToString(libr );
+				var centi = (dato2 * 100) / 1;
+				centimetros.Text = Convert.ToString(centi);
+			}
+			else
+			{
+				DisplayAlert("Error", "Escribe el peso y la altura", "Se le agradece");
+			}
+		}
+	}
+
+
